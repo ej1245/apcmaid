767ffe5db15a74500ab7a212445f3a6fba1c8c1e
ej1245 <39335437+ej1245@users.noreply.github.com>
2025-09-06 18:42:28 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,5 @@
+       <?php
+    header("HTTP/1.1 301 Moved Permanently");
+    header("Location: https://www.apcmaid.com/computer-repair-rochester-mn/");
+    exit(); // It's good practice to exit after a redirect
+    ?>
